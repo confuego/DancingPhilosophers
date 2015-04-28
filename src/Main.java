dfd214caa49e20cc1f1de7058d3a40fe3ece665f
confuego <wes.chappell10@gmail.com>
2015-04-28 12:35:30 -0400

    changed some things...

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ public class Main {
 	public static void main(String[] args) {
 		Hashtable<String,Long> h = new Hashtable<String,Long>(8);
 		h.put("Mamba", 100L);
-		Thread t1 = new Thread(new DanceCard(new Hashtable<String,Long>(8)),"L");
-		Thread t2 = new Thread(new DanceCard(h),"F");
+		Thread t1 = new Thread(new DanceCard(new Hashtable<String,Long>(8),2),"L");
+		Thread t2 = new Thread(new DanceCard(h,2),"F");
 		h.put("weiner", 50L);
-		Thread t3 = new Thread(new DanceCard(h),"F");
+		Thread t3 = new Thread(new DanceCard(h,2),"F");
 		t2.start();t1.start();t3.start();
 		try {t1.join();t2.join();t3.join();} 
 		catch (InterruptedException e) {e.printStackTrace();}
