69c3983b3452c65f202d66e9aa8f225c45823c7d
Schuyler Cumbie <schuylerc@gmail.com>
2015-05-03 21:16:28 -0400

    added a default for missing command line args

--- src/Buffer.java
+++ src/Buffer.java
@@ -1,37 +1,48 @@
 public class Buffer {
 	Follower[] vals;
-	int low,high,size;
-	public Buffer(int len){
+	int low, high, size;
+
+	public Buffer(int len) {
 		vals = new Follower[len];
 	}
-	
-	public synchronized void put(Follower v){
-		while(size==vals.length){
-			try{this.wait();}catch(InterruptedException e){e.printStackTrace();}
+
+	public synchronized void put(Follower v) {
+		while (size == vals.length) {
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		vals[high] = v;
-		high = (high+1)%vals.length;
+		high = (high + 1) % vals.length;
 		size++;
 		notifyAll();
 	}
-	public synchronized void update(Follower f){
-		int x=0;
-		for(Follower val : vals){
-			if(vals[x].equals(f)){
+
+	public synchronized void update(Follower f) {
+		int x = 0;
+		for (Follower val : vals) {
+			if (vals[x].equals(f)) {
 				vals[x] = val;
 				notifyAll();
 			}
 			x++;
 		}
 	}
-	public synchronized Follower get(){
+
+	public synchronized Follower get() {
 		Follower v;
-		
-		while(size<=0){
-			try{wait();}catch(InterruptedException e){e.printStackTrace();}
+
+		while (size <= 0) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		v = vals[low];
-		low = (low+1)%vals.length;
+		low = (low + 1) % vals.length;
 		size--;
 		notifyAll();
 		return v;
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,55 @@
 public class Main {
 
 	public static void main(String[] args) {
-		int numLeaders = Integer.parseInt(args[0]);
-		int numFollowers = Integer.parseInt(args[1]);
-		int count = 1;
-		
-		
+
+		int numLeaders, numFollowers, count;
+
+		if (args.length < 2) {
+			System.err.println("Defaulting to 2 leaders and 2 followers");
+			numLeaders = 2;
+			numFollowers = 2;
+		} else {
+			numLeaders = Integer.parseInt(args[0]);
+			numFollowers = Integer.parseInt(args[1]);
+		}
+
+		count = 1;
+
 		Leader[] leaderList = new Leader[numLeaders];
 		Follower[] followerList = new Follower[numFollowers];
 		Buffer b = new Buffer(numFollowers);
-		
-		while(count <= numLeaders){
-			leaderList[count-1] = new Leader(b,count,numFollowers);
+
+		while (count <= numLeaders) {
+			leaderList[count - 1] = new Leader(b, count, numFollowers);
 			count++;
 		}
 		count = 1;
-		
-		while(count <= numFollowers){
-			followerList[count-1] = new Follower(b,count,numLeaders);
+
+		while (count <= numFollowers) {
+			followerList[count - 1] = new Follower(b, count, numLeaders);
 			count++;
 		}
-		
-		for(Leader l : leaderList){l.start();}
-		for(Follower f : followerList){f.start();}
-		try{for(Leader l : leaderList){l.join();}for(Follower f : followerList){f.join();}}catch (InterruptedException e){e.printStackTrace();}
-		for(Leader l : leaderList){l.printTable();System.out.println();}
+
+		for (Leader l : leaderList) {
+			l.start();
+		}
+		for (Follower f : followerList) {
+			f.start();
+		}
+		try {
+			for (Leader l : leaderList) {
+				l.join();
+			}
+			for (Follower f : followerList) {
+				f.join();
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		for (Leader l : leaderList) {
+			l.printTable();
+			System.out.println();
+		}
 	}
 
 }
