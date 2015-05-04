356ee7e1dae0232b062bb2dc26cffa06222e8a01
Schuyler Cumbie <schuylerc@gmail.com>
2015-05-04 15:44:09 -0400

    update leader and follower to contain this new dance card object as a field

--- src/Follower.java
+++ src/Follower.java
@@ -1,17 +1,22 @@
 import java.util.Hashtable;
+
 public class Follower extends Thread {
-	Buffer buff;
-	int id;
-	Hashtable<String,Integer> danceCard = new Hashtable<String,Integer>(8);
-	int leaderCount;
-	
-	public Follower(Buffer b, int id,int leaderCount){
+	public Buffer buff;
+	public int id;
+	public Hashtable<String, Integer> danceCard = new Hashtable<String, Integer>(
+			8);
+	public int leaderCount;
+	public DanceCard card;
+
+	public Follower(Buffer b, int id, int leaderCount) {
+		this.card = new DanceCard();
 		buff = b;
 		this.id = id;
 		this.leaderCount = leaderCount;
 	}
-	
-	public void run(){
+
+	@Override
+	public void run() {
 		danceCard.put("Waltz", -1);
 		danceCard.put("Tango", -1);
 		danceCard.put("Foxtrot", -1);
@@ -20,7 +25,7 @@ public class Follower extends Thread {
 		danceCard.put("Samba", -1);
 		danceCard.put("Cha Cha", -1);
 		danceCard.put("Jive", -1);
-		for(int x=0;x<leaderCount;x++){
+		for (int x = 0; x < leaderCount; x++) {
 			buff.put(this);
 		}
 	}
--- src/Leader.java
+++ src/Leader.java
@@ -1,17 +1,22 @@
 import java.util.Hashtable;
-public class Leader extends Thread{
-	Hashtable<String,Integer> danceCard = new Hashtable<String,Integer>(8);
-	Buffer buff;
-	int followerCount;
-	int id;
-	
-	public Leader(Buffer b,int id,int followerCount){
+
+public class Leader extends Thread {
+	public Hashtable<String, Integer> danceCard = new Hashtable<String, Integer>(
+			8);
+	public Buffer buff;
+	public int followerCount;
+	public int id;
+	public DanceCard card;
+
+	public Leader(Buffer b, int id, int followerCount) {
+		this.card = new DanceCard();
 		this.followerCount = followerCount;
 		buff = b;
 		this.id = id;
 	}
-	
-	public void run(){
+
+	@Override
+	public void run() {
 		danceCard.put("Waltz", -1);
 		danceCard.put("Tango", -1);
 		danceCard.put("Foxtrot", -1);
@@ -20,53 +25,54 @@ public class Leader extends Thread{
 		danceCard.put("Samba", -1);
 		danceCard.put("Cha Cha", -1);
 		danceCard.put("Jive", -1);
-		for(int x=0;x<followerCount;x++){
-			//synchronized(this){
-				Follower f = buff.get();
-				//System.out.println("Leader " + id + " got Follower " + f.id);
-				//f.danceCard.put(String.valueOf(id),f.id);
-				this.compare(f);
-				//System.out.println("Leader " + id + " added " + f.danceCard + " to Follower " + f.id);
-			//}
+		for (int x = 0; x < followerCount; x++) {
+			// synchronized(this){
+			Follower f = buff.get();
+			// System.out.println("Leader " + id + " got Follower " + f.id);
+			// f.danceCard.put(String.valueOf(id),f.id);
+			this.compare(f);
+			// System.out.println("Leader " + id + " added " + f.danceCard +
+			// " to Follower " + f.id);
+			// }
 		}
 	}
-	
-	public synchronized void compare(Follower f){
-		String[] keyArray = {"Waltz","Tango","Foxtrot","Quickstep","Rumba","Samba","Cha Cha","Jive"};
+
+	public synchronized void compare(Follower f) {
+		String[] keyArray = { "Waltz", "Tango", "Foxtrot", "Quickstep",
+				"Rumba", "Samba", "Cha Cha", "Jive" };
 		int danceCount = 0;
-		for(String key  : f.danceCard.keySet()){
-			if(this.danceCard.get(key) == f.id){
+		for (String key : f.danceCard.keySet()) {
+			if (this.danceCard.get(key) == f.id) {
 				danceCount++;
 			}
 		}
-		int count =0;
-		while(danceCount < 2 && count != 8){
+		int count = 0;
+		while (danceCount < 2 && count != 8) {
 			int rand = (int) (Math.random() * 7);
 			String key = keyArray[rand];
-			//System.out.println("Key is: " + key);
-			if(this.danceCard.get(key) == -1 && f.danceCard.get(key) == -1){
-				this.danceCard.put(key, f.id);f.danceCard.put(key, this.id);
+			// System.out.println("Key is: " + key);
+			if (this.danceCard.get(key) == -1 && f.danceCard.get(key) == -1) {
+				this.danceCard.put(key, f.id);
+				f.danceCard.put(key, this.id);
 				danceCount++;
 			}
 			count++;
 		}
 		notifyAll();
 	}
-	
-	public synchronized void printTable(){
-		System.out.println("Leader: " + this.id);
-		for(String key : this.danceCard.keySet()){
-			if(key.equals("Quickstep") && this.danceCard.get(key)==-1){
+
+	public synchronized void printTable() {
+		System.out.println("Leader " + this.id + ":");
+		for (String key : this.danceCard.keySet()) {
+			if (key.equals("Quickstep") && this.danceCard.get(key) == -1) {
 				System.out.println(key + "\t------");
-			}
-			else if(this.danceCard.get(key) == -1)
+			} else if (this.danceCard.get(key) == -1)
 				System.out.println(key + "\t\t" + "------");
-			else if(key.equals("Quickstep") && this.danceCard.get(key)!=-1){
+			else if (key.equals("Quickstep") && this.danceCard.get(key) != -1) {
 				System.out.println(key + "\twith " + this.danceCard.get(key));
-			}
-			else
+			} else
 				System.out.println(key + "\t\twith " + this.danceCard.get(key));
 		}
 	}
-	
+
 }
