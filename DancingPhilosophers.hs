137859e059717053c8d89964452535b15a8ed4e4
confuego <wes.chappell10@gmail.com>
2015-05-06 02:25:09 -0400

    Updated Haskell Version

--- DancingPhilosophers.hs
+++ DancingPhilosophers.hs
@@ -12,6 +12,12 @@ type DanceCard = [(String,Int)]
 
 danceCard = [("Waltz",-1),("Tango",-1),("Foxtrot",-1),("Quickstep",-1),("Rumba",-1),("Samba",-1),("Cha Cha",-1),("Jive",-1)]
 
+takefollower follower = takeTMVar follower
+putfollower follower table = putTMVar follower (table)
+
+randomDelay max = do
+	n <- getStdRandom (randomR (1,max))
+	threadDelay (n*100000)
 
 put :: String -> Int -> DanceCard -> DanceCard
 put key value table = put' key value table 0
@@ -40,10 +46,67 @@ printTable ((dance,id):table)
 	| (id == -1) = if(dance == "Quickstep") then ((dance ++ "\t with ------\n") ++ printTable table) else ((dance ++ "\t\t with ------\n") ++ printTable table)
 	| otherwise =  if(dance == "Quickstep") then ((dance ++ "\t with "++ (show id) ++"\n") ++ printTable table) else (dance ++ "\t\t with "++ (show id) ++"\n") ++ printTable table
 
+
+createLeaders ::Int -> Int -> TVar Int -> DanceCard -> TChan String -> TMVar(DanceCard) -> IO ()
+createLeaders 0 numFollowers donecount table msgs buffer = return ()
+createLeaders n numFollowers donecount table msgs buffer = do 
+	let tableCopy = table
+	leader <- atomically $ newTMVar (tableCopy)
+	atomically $ do 
+		l <- takeTMVar leader
+		putTMVar leader (put "Samba" n l)
+	forkIO $ compareToFollower ("Leader " ++ (show n)) leader msgs numFollowers donecount buffer
+	createLeaders (n-1) numFollowers donecount table msgs buffer
+
+createFollowers :: Int -> Int -> TVar Int -> DanceCard -> TMVar(DanceCard) -> TChan String -> IO ()
+createFollowers 0 numLeaders donecount table buffer msgs = return ()
+createFollowers n numLeaders donecount table buffer msgs = do
+	let tableCopy = table
+	forkIO $ tryBuffer buffer tableCopy msgs
+	createFollowers (n-1) numLeaders donecount table buffer msgs
+
+tryBuffer :: TMVar(DanceCard) -> DanceCard -> TChan String -> IO ()
+tryBuffer buffer table outs = do
+	rand <- getStdRandom (randomR (1,100)) 
+	atomically $ do putfollower buffer (put "Cha Cha" rand table)
+	randomDelay 6
+	return ()
+
+compareToFollower :: String  -> TMVar(DanceCard) -> TChan String -> Int -> TVar Int -> TMVar(DanceCard) -> IO()
+compareToFollower name table outs followerCount donecount buffer = do
+	atomically $ do
+		t <- takeTMVar table
+		ft <- takefollower buffer
+		writeTChan outs $ name ++ " is looking at follower in buffer: \n" ++ (show ft) ++ "\n"
+	randomDelay 6
+	return ()
+
+shoutLeader :: TChan String -> TVar Int -> Int -> IO()
+shoutLeader msgs followerCount totalFollowers = do
+	fcount <- atomically $ readTVar followerCount
+	empty <- atomically $ isEmptyTChan msgs
+	if(( not(empty) ) && fcount < totalFollowers)
+		then do
+			msg <- atomically $ readTChan msgs
+			putStrLn msg
+			shoutLeader msgs followerCount totalFollowers
+	else return ()
+
 main :: IO()
 main = do
-	putStrLn (show (put "Tango" 5 danceCard))
-	--putStrLn (printLeader 1 danceCard_1)
-	putStrLn (show (put "Tango" 50 danceCard))
-	putStrLn (show (get "Tango" danceCard))
-	--putStrLn (printLeader 2 danceCard_2)
+	args <- getArgs
+	let tableCopy = danceCard
+	buffer <- atomically $ newTMVar (tableCopy)
+	msgs <- atomically newTChan
+	donecount <- newTVarIO (0::Int)
+	let numLeaders = read (args !! 0) :: Int
+	let numFollowers = read (args !! 1) :: Int
+	createLeaders numLeaders numFollowers donecount danceCard msgs buffer
+	createFollowers numFollowers numLeaders donecount danceCard buffer msgs
+	forkIO $ shoutLeader msgs donecount 2
+	atomically $ do
+		count <- readTVar donecount
+		check $ count == 0
+		writeTChan msgs $ "main waiting.............."
+	threadDelay (5*1000000)
+	atomically $ writeTChan msgs "main done!!!!!!!!!!!"	
